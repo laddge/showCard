3bec02fc7b004df304fcbbebfa8d0ed1cb69201f
laddge <dev.laddge@gmail.com>
2021-02-04 22:22:48 +0900

    update js and css

--- showCard.js
+++ showCard.js
@@ -7,21 +7,16 @@ for (let i = 0; i < links.length; i++) {
     })
     .then(json => {
       if (json.image != '') {
-        links[i].innerHTML = '<img src="' + json.image + '" class="card_image">';
-      } else {
-        links[i].innerHTML = '';
-      }
-      if (json.meta_title != '') {
-        links[i].innerHTML = links[i].innerHTML + '<p class="card_title">' + json.meta_title + '</p>';
-      } else if (json.title != '') {
-        links[i].innerHTML = links[i].innerHTML + '<p class="card_title">' + json.title + '</p>';
-      }
-      if (json.description != '') {
-        desc = json.description;
-        if (desc.length > 50) {
-          desc = desc.slice(0, 50) + '……';
+        if (json.title != '') {
+          if (json.description != '') {
+            desc = json.description;
+            if (desc.length > 50) {
+              desc = desc.slice(0, 50) + '……';
+            }
+            links[i].innerHTML = '<img src="' + json.image + '" class="card_image"><p class="card_title">' + json.title + '</p><p class="card_description">' + desc + '</p>';
+            links[i].classList.add('card_box');
+          }
         }
-        links[i].innerHTML = links[i].innerHTML + '<p class="card_description">' + desc + '</p>';
-      }
+      }  
     });
 }
